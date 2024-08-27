313598e233d0b6fb882e8f595190f4d920fc24a4
kentaro <kentaro523@gmail.com>
2024-08-27 22:46:11 +0900

    修正

--- components/CategoryList/index.tsx
+++ components/CategoryList/index.tsx
@@ -12,14 +12,13 @@ export const CategoryList: FC<Props> = ({ categories, selectedCategoryId }) => {
     <ul>
       <div className="text-2xl mb-4 underline">カテゴリ一覧</div>
       {categories.map((category, i) => (
-        <li
-          key={i}
-          className={`py-3 cursor-pointer hover:bg-gray-500 text-xl list-none ${selectedCategoryId === category.id ? 'bg-gray-500' : ''}`}
-        >
-          <Link href={`/articles/category/${category.id}/1`}>
+        <Link key={i} href={`/articles/category/${category.id}/1`}>
+          <li
+            className={`py-3 cursor-pointer hover:bg-gray-500 text-xl list-none ${selectedCategoryId === category.id ? 'bg-gray-500' : ''}`}
+          >
             {category.name}
-          </Link>
-        </li>
+          </li>
+        </Link>
       ))}
     </ul>
   )
--- components/TopPage/index.tsx
+++ components/TopPage/index.tsx
@@ -34,10 +34,10 @@ export const TopPage: FC = () => {
         />
         <div className="relative inset-y-0 left-0 flex w-full flex-col md:pl-40">
           <div className="text-center text-2xl font-extrabold md:text-left md:text-5xl">
-            Any Vision, Any Solution
+            For the Future, Any Digital Solution!
           </div>
           <div className="text-md text-center font-bold md:mt-4 md:text-left md:text-3xl">
-            どんな目標にも、あらゆる解決策を
+            デジタルで紡ぐ、未来の物語
           </div>
         </div>
       </div>
